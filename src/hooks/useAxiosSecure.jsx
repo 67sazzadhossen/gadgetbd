18721641664573388a983474541c2936ec6b01f4
67sazzadhossen <sazzadhossen010@gmail.com>
2024-11-19 13:04:39 +0600

    c

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,6 +3,9 @@ import axios from "axios";
 const axiosSecure = axios.create({
   baseURL: "https://gadgetbd-server.vercel.app",
 });
+// const axiosSecure = axios.create({
+//   baseURL: "http://localhost:3000",
+// });
 
 const useAxiosSecure = () => {
   return axiosSecure;
