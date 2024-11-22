da85e8c0df9eafb438e2e7754492378caa369cdf
67sazzadhossen <sazzadhossen010@gmail.com>
2024-11-22 20:59:07 +0600

    c

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -3,14 +3,14 @@ import { useContext, useEffect } from "react";
 import { AuthContext } from "../Providers/AuthProvider";
 import { useNavigate } from "react-router";
 
-// const axiosSecure = axios.create({
-//   baseURL: "https://gadgetbd-server.vercel.app",
-//   withCredentials: true,
-// });
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://gadgetbd-server.vercel.app",
   withCredentials: true,
 });
+// const axiosSecure = axios.create({
+//   baseURL: "http://localhost:3000",
+//   withCredentials: true,
+// });
 
 const useAxiosSecure = () => {
   // const { Logout } = useContext(AuthContext);
