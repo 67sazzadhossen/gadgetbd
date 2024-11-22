ffa23ddad1f588ced05f0730f4f08ade8a5a242a
67sazzadhossen <sazzadhossen010@gmail.com>
2024-11-22 20:02:54 +0600

    c

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
   baseURL: "https://gadgetbd-server.vercel.app",
+  withCredentials: true,
 });
 // const axiosSecure = axios.create({
 //   baseURL: "http://localhost:3000",
