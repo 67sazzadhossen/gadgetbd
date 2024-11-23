a9ce74fc09193eec0d4143efa7b71142578823aa
67sazzadhossen <sazzadhossen010@gmail.com>
2024-11-23 09:10:40 +0600

    c

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -3,13 +3,16 @@ import DashNav from "../components/DashNav";
 
 const DashboardLayout = () => {
   return (
-    <div className="grid grid-cols-12 container mx-auto ">
-      <div className="col-span-3 relative ">
-        <div className="w-[20%] fixed">
+    <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 lg:grid-cols-12 gap-4">
+      {/* Sidebar */}
+      <div className="hidden md:block md:col-span-1 lg:col-span-3 relative">
+        <div className="w-full lg:w-[20%] fixed">
           <DashNav />
         </div>
       </div>
-      <div className="col-span-9">
+
+      {/* Main Content */}
+      <div className="col-span-1 md:col-span-3 lg:col-span-9">
         <Outlet />
       </div>
     </div>
